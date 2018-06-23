b1171b3675d32a8026d0bc1b90c38bf9b6fa8e14
Sophia <phuongmai001@gmail.com>
2018-06-22 19:59:46 -0700

    update the whole app

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-const pixelCanvas = $('#pixelCanvas');
+// IMPORTANT: Wrap your code in a document.ready function
+// i.e allow the dom to load before doing anything with jquery
 
-function makeGrid() {
-  pixelCanvas.find('tr').remove(); // reset to blank
-  const column = $('#inputHeight').val();
-  const row = $('#inputWidth').val();
+// otherwise you have to put this in the html script tag
 
-  //Create the grid
-  let grid;
-  for (i = 0; i < column; i++ ){
-    grid = grid + '<tr>';
-    for (j = 0; j < row; j++) {
-      grid = grid + '<td></td>';
+$(document).ready(function() {
+  const pixelCanvas = $('#pixelCanvas');
+
+  function makeGrid() {
+    pixelCanvas.find('tr').remove(); // reset to blank
+    const column = $('#inputHeight').val();
+    const row = $('#inputWidth').val();
+
+    //Create the grid
+    let grid; 
+    for (i = 0; i < column; i++ ){
+      grid = grid + '<tr>';
+      for (j = 0; j < row; j++) {
+        grid = grid + '<td></td>';
+      }
+      grid = grid + '</tr>';
     }
-    grid = grid + '</tr>';
+    pixelCanvas.html(grid);
   }
-  pixelCanvas.html(grid);
-}
 
-// create table when user click on submit button
-$('#sizePicker').on('submit', function(e){
-  e.preventDefault();
-  $(makeGrid())
-})
+  // create table when user click on submit button
+  $('#sizePicker').on('submit', function(e){
+    e.preventDefault();
+    $(makeGrid())
+  })
 
-// Add color to the pixel
-pixelCanvas.on('click', 'td', function () {
-  const color = $('#colorPicker').val();
-  $(this).css('background-color', color);
-});
+  // Add color to the pixel
+  pixelCanvas.on('click', 'td', function () {
+    const color = $('#colorPicker').val();
+    $(this).css('background-color', color);
+  });
 
-// Remove color by double click
-pixelCanvas.on('dblclick', 'td', function () {
-  $(this).css('background-color', 'white');
+  // Remove color by double click
+  pixelCanvas.on('dblclick', 'td', function () {
+    $(this).css('background-color', 'white');
+  });
 });
